4af2b812d844c583026ff453555d457b88e1cffa
Steve Robles <steve@icon-media.com>
2024-07-15 20:51:06 -0700

    creating home page layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import RecipePage from './pages/RecipePage.js';
 import HomePage from './pages/HomePage';
+import TastyRoulettePage from './pages/TastyRoulettePage.js';
+import Navbar from './components/Navbar';
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/recipes" element={<RecipePage/>} />
+        <Route path="/tasty-roulette" element={<TastyRoulettePage/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,40 @@
 import '../styles/HomePage.css';
-import Navbar from '../components/Navbar'; 
+import '../styles/HomePage.css'
 
 function HomePage() {
     return ( 
-        <div>
-            <Navbar/>
-            <div className='home'>
-                <div className='card'>
-                    <h3>Breakfast</h3>
-                </div>
-                <div className='card'>
-                    <h3>Lunch</h3>
-                </div>
-                <div className='card'>
-                    <h3>Dinner</h3>
-                </div>
+<div className="container text-center mt-5">
+      <div className="row">
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="" className="card-img-top" alt="Breakfast" />
+            <div className="card-body">
+              <h5 className="card-title">Breakfast</h5>
+              <p className="card-text">This is a description of the breakfast dish.</p>
             </div>
-        </div>    
+          </div>
+        </div>
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="" className="card-img-top" alt="Lunch" />
+            <div className="card-body">
+              <h5 className="card-title">Lunch</h5>
+              <p className="card-text">This is a description of the lunch dish.</p>
+
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="" className="card-img-top" alt="Dinner" />
+            <div className="card-body">
+              <h5 className="card-title">Dinner</h5>
+              <p className="card-text">This is a description of the dinner dish.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     );
 };
 
